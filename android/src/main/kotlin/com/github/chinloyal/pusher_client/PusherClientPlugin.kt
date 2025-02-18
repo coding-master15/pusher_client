f00b307c28a8976554a74bd743e44af4fd057a2e
Anas Nadeem <anasnadeem200406@gmail.com>
2025-02-18 13:56:24 +0500

    changes

--- android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
+++ android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
@@ -2,28 +2,22 @@ package com.github.chinloyal.pusher_client
 
 import androidx.annotation.NonNull
 import com.github.chinloyal.pusher_client.pusher.PusherService
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** PusherClientPlugin */
-class PusherClientPlugin: FlutterPlugin, MethodCallHandler {
-
-  private var pusherService: PusherService? = null
+class PusherClientPlugin: FlutterPlugin {
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    pusherService = PusherService()
-    pusherService?.register(flutterPluginBinding.binaryMessenger)
+    PusherService().register(flutterPluginBinding.binaryMessenger);
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    pusherService?.unregister()  // Ensure cleanup
-    pusherService = null
-  }
-
-  fun unregister() {
-    channel?.setMethodCallHandler(null)  // Detach method call handling
+//    channel.setMethodCallHandler(null)
   }
 }
